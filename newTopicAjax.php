62cf1efa6e211cb4e58b05485d30f3bfe588a94e
naghiLiviu <liviu.naghi93@gmail.com>
2015-10-15 16:51:11 +0300

    topic.php and newTopicAjax.php modified

--- newTopicAjax.php
+++ newTopicAjax.php
@@ -1,51 +1,14 @@
 <?php
-session_start();
-$mysqli = new mysqli ('localhost' , 'root' , 'root' , 'ForumIT');
-if ($_POST) {
-    if (isset($_SESSION['userId']) || $_SESSION['userId'] != null) {
-        if (!empty ($_POST['newTopic']) && !empty($_POST['newComment'])) {
-            $newTopic = $_POST['newTopic'];
-            $sectionId = $_GET["sectionId"];
-            $newComment = $_POST ['newComment'];
-            $userId = $_SESSION['userId'];
 
-            $_SESSION['message'] = "Topic $newTopic inserted";
-            $mysqli->query("INSERT INTO Topic (SectionId, TopicName) VALUES ('$sectionId', '$newTopic')");
-            $lastTopicId = $mysqli->insert_id;
-            $mysqli->query("INSERT INTO Comment (UserId, TopicId, Comment) VALUES ('$userId','$lastTopicId','$newComment')");
-            header("Location: comment.php?topicId=$lastTopicId");
-        } else {
-            $_SESSION['message'] = "Please fill in all the fields";
-        }
-        echo $_SESSION['message'];
-        $_SESSION['message'] = null;
-    }
-}
-?>
-<p> Post a new topic: </p>
-<br>
+$mysqli = new mysqli ('localhost', 'root', 'root', 'ForumIT');
 
-<form method="post" onsubmit="countWords()" name="submitTopic">
-    <label>
-        <input type="text" name="newTopic">
-    </label><br>
+$newTopic = $_POST['newTopicName'];
+$sectionId = $_POST["sectionId"];
+$newComment = $_POST ['newTopicComment'];
+$userId = $_POST['userId'];
 
-    <p> Post a comment: </p>
-    <label>
-        <textarea name="newComment" rows="10" cols="40"></textarea>
-    </label><br><br>
+$mysqli->query("INSERT INTO Topic (SectionId, TopicName) VALUES ('$sectionId', '$newTopic')");
 
+$lastTopicId = $mysqli->insert_id;
 
-    <input type="submit" name="submit" value="Post" class="button1">
-</form>
-<script>
-    function countWords() {
-        var comment = document.forms["submitTopic"]["newComment"].value;
-
-        if (comment.length < 20) {
-            alert("Your comment is too short!");
-            return false;
-        }
-    }
-</script>
-
+$mysqli->query("INSERT INTO Comment (UserId, TopicId, Comment) VALUES ('$userId','$lastTopicId','$newComment')");
--- topic.php
+++ topic.php
@@ -11,44 +11,18 @@ $resultTopic = $mysqli->query($sqlTopic);
 <div class="container">
     <?php require("header.php"); ?>
     <div class="content">
-        <div id="newTopic">
-                <?php if ($isSetUserId) {
-                    echo '<button id="button"  class="button1" > Post a new topic </button >';
-                } ?>
+        <?php if ($isSetUserId) {
+            echo '<button id="newTopicButton" class="button1" > Post a new topic </button >';
+        } ?>
+        <div id="hiddenForm">
+            <p>New topic name:</p>
+            <input type="text" id="newTopicName" title="inputNewTopicName">
+            <br><br>
+            <p>Please insert a comment:</p>
+            <textarea id="newTopicComment" title="textareaNewTopicComment"></textarea>
+            <br><br>
+            <button id="submitNewTopic">Submit</button>
         </div>
-        <script>
-            $(document).ready(function () {
-                    var sectionId = <?php echo $_GET["sectionId"]; ?>;
-
-                $("#button").click(function () {
-                        $.ajax({
-                            type: "POST",
-                            url: "newTopicAjax.php",
-                            data: {
-                                sectionId: sectionId
-                            },
-                            dataType: 'html',
-                            success: function () {
-                                $("#newTopic").load("newTopicAjax.php") ;
-                            },
-                            error: function() {
-                                alert('Error processing the request');
-                            }
-
-                        });
-                    }
-                    )
-                    ;
-                });
-        </script>
-
-        <div>
-            <?php
-            include ("explainSection.php");
-            ?>
-
-        </div>
-
         <table>
             <tr>
                 <th>Topics</th>
@@ -57,34 +31,72 @@ $resultTopic = $mysqli->query($sqlTopic);
             </tr>
             <?php
             foreach ($resultTopic as $topicKey => $topicValue) {
-            $topicId = $topicValue["TopicId"];
+                $topicId = $topicValue["TopicId"];
 
-            echo "<tr><td><a href=\"comment.php?topicId=$topicId\">" . $topicValue["TopicName"] . "</a></td>";
-            $sqlComment = "SELECT * FROM Comment
+                echo "<tr><td><a href=\"comment.php?topicId=$topicId\">" . $topicValue["TopicName"] . "</a></td>";
+                $sqlComment = "SELECT * FROM Comment
                               LEFT JOIN User ON Comment.UserId = User.UserId
                               WHERE Comment.TopicId = '$topicId'AND Comment.CommentStatus='Active'
                               GROUP BY Comment.TopicId
                               ORDER BY Comment.CommentId DESC";
-            $resultComment = $mysqli->query($sqlComment);
-            $countComment = $resultComment->num_rows;
-            $lastUserComment = array();
-            foreach($resultComment as $commentKey => $commentValue){
-            $lastUserComment[] = $commentValue;
-            }
-            echo "<td>" . $countComment . "</td>";
-            echo '<td>' . $lastUserComment[0]["UserName"] . '</td>';
-            if ($_SESSION["roleId"] == 1 || $_SESSION["roleId"] == 2 ) {
-            echo "<td><a href=\"editTopic.php?topicId=$topicId\">Edit </a></td>";
-            echo '<td><button class="deleteButton" onclick="deleteFunction(' . $topicId . ')" id="demo">Delete</button></td></tr>';
-            } else {
-            echo "</tr>";
-            }
+                $resultComment = $mysqli->query($sqlComment);
+                $countComment = $resultComment->num_rows;
+                $lastUserComment = array();
+                foreach($resultComment as $commentKey => $commentValue){
+                    $lastUserComment[] = $commentValue;
+                }
+                echo "<td>" . $countComment . "</td>";
+                echo '<td>' . $lastUserComment[0]["UserName"] . '</td>';
+                if ($_SESSION["roleId"] == 1 || $_SESSION["roleId"] == 2 ) {
+                    echo "<td><a href=\"editTopic.php?topicId=$topicId\">Edit </a></td>";
+                    echo '<td><button class="deleteButton" onclick="deleteFunction(' . $topicId . ')" id="demo">Delete</button></td></tr>';
+                } else {
+                    echo "</tr>";
+                }
             }
             ?>
         </table>
     </div>
     <?php require("footer.php"); ?>
 </div>
+
+<script>
+    $(document).ready(function () {
+        $('#hiddenForm').hide();
+        $('#newTopicButton').on('click', function () {
+            $('#hiddenForm').show();
+            $("#newTopicButton").hide();
+        });
+        $('#submitNewTopic').on('click', function () {
+            $('#hiddenForm').hide();
+        })
+    });
+</script>
+
+<script>
+    $(document).ready(function () {
+        var userId = <?php echo $_SESSION["userId"];?>;
+        var sectionId = <?php echo $_GET["sectionId"]; ?>;
+        $("#submitNewTopic").click(function () {
+            var newTopicComment = document.getElementById("newTopicComment").value;
+            var newTopicName = document.getElementById("newTopicName").value;
+            $.ajax({
+                method: "POST",
+                url: "newTopicAjax.php",
+                data: {
+                    newTopicName: newTopicName,
+                    newTopicComment: newTopicComment,
+                    userId: userId,
+                    sectionId: sectionId
+                },
+                success: function () {
+                    location.reload();
+                }
+            });
+        });
+    });
+</script>
+
 <script>
     function deleteFunction(id) {
         if (confirm("Are you sure you want to delete this topic?") == true) {
@@ -96,4 +108,3 @@ $resultTopic = $mysqli->query($sqlTopic);
 
 </script>
 </body>
-</html>
