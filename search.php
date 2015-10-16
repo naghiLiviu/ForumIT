8c6467d11044ea0d14ee680e0cb55f18e180e178
Isabela09 <branescu.isabela@gmail.com>
2015-10-16 11:51:20 +0300

    search modificat

--- search.php
+++ search.php
@@ -1,11 +1,17 @@
 <?php
 require_once('common.php');
-//I am here!!!
+
 ?>
 <body class="mainbody">
 <div class="container">
     <?php require("header.php"); ?>
-    <div class="regform">
+    <div class="regform" >
+        <?php
+        if (!empty ($_GET['name'])){
+        if ($countResult == 0) {
+            echo "Nothing found ";
+        } else {
+        ?>
         <table border="1">
             <tr>
                 <th>Section Name</th>
@@ -13,26 +19,35 @@ require_once('common.php');
                 <th>Posts</th>
             </tr>
             <?php
+
             $searchTopic = $_GET['name'];
             $sqlString = "SELECT * FROM Topic
             LEFT JOIN Section
             ON Topic.SectionId=Section.SectionId
             WHERE TopicName LIKE  '%$searchTopic%' OR SectionName LIKE '%$searchTopic%'  ";
             $search = $mysqli->query($sqlString);
+            //numaram rezultatele
+            $countResult = $search->num_rows;
+
             foreach ($search as $row) {
                 $topicId = $row["TopicId"];
                 $sqlComment = "SELECT * FROM Comment WHERE TopicId='$topicId'";
                 $resultComment = $mysqli->query($sqlComment);
                 $countComment = $resultComment->num_rows;
+
                 ?>
                 <tr>
                     <td><?php echo $row['SectionName']; ?></td>
-                    <td><a href="comment.php?topicId=<?php echo $topicId; ?>"><?php echo $row['TopicName']; ?></a></td>
+                    <td><a href="comment.php?topicId=<?php echo $topicId; ?>"><?php echo $row['TopicName']; ?></a>
+                    </td>
                     <td><?php echo $countComment; ?></td>
                 </tr>
 
                 <?php
             }
+            }
+            }
+            else {echo "please insert your search word";}
             ?>
         </table>
     </div>
