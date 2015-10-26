d1f0486698f34f2aba715ababf14060313b72cc1
naghiLiviu <liviu.naghi93@gmail.com>
2015-10-26 12:15:10 +0200

    header MVC added

---
+++ Controller/HeaderController.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../Utils/sessions.php';
+
+$registerLink = '';
+$loginLink = '';
+$logoutLink = '';
+if (!isset($_SESSION['userId']) && $_SESSION['userId'] == null) {
+    $registerLink = '<a href="../View/register.php">Register</a> | ';
+    $loginLink = '<a href="../View/login.php">LogIn</a>';
+}else{
+    $logoutLink = '<a href ="../logout.php">Log out</a>';
+}
