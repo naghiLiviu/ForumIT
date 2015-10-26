a753b020743f7a5ab17a3761cdae8022855c820b
Isabela09 <branescu.isabela@gmail.com>
2015-10-26 13:35:44 +0200

    index

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -20,10 +20,6 @@ $section = new Section();
 $topic = new Topic ();
 $role = new Role();
 
-
-//$roleId = $_SESSION['roleId'];
-//$sqlSection = "SELECT * FROM Section";
-
 $resultSection = $section->getSection();
 $sections = array();
 foreach ($resultSection as $sectionKey => $sectionValue) {
@@ -33,39 +29,34 @@ foreach ($resultSection as $sectionKey => $sectionValue) {
     $resultTopic = $topic->getTopic($sectionValue["SectionId"]);
     $countTopic = $resultTopic->num_rows;
 
-
     if ($sectionValue["SectionStatus"] == "Active") {
         if ($_SESSION["roleId"] == Role::ADMIN || $_SESSION["roleId"] == Role::MODERATOR) {
             $editLink = '<a href="../View/editSection.php">Edit</a>';
+
+            if ($_SESSION["roleId"] == Role::ADMIN || $_SESSION["roleId"] == Role::MODERATOR) {
+                $deleteLink = '<button class="deleteButton" onclick="deleteFunction(' . $sectionId . ')">Delete</button>';
+            }
         }
-        if ($_SESSION["roleId"] == Role::ADMIN || $_SESSION["roleId"] == Role::MODERATOR) {
-            $deleteLink = '<button class="deleteButton" onclick="deleteFunction(' . $sectionId . ')">Delete</button>';
+
+        $countPosts = 0;
+        $topicArray = array();
+        foreach ($resultTopic as $topicKey => $topicValue) {
+            $topicId = $topicValue["TopicId"];
+            $topicArray[] = $topicValue;
+            $countPosts = $comment->countCommentsByTopicId($topicValue["TopicId"]);
+
+            $countPosts += $countPosts;
+
         }
+        $lastPost = $topicArray[0]["UserName"];
+        $sectionRow = array(
+            'Section' => $sectionValue['SectionName'],
+            'Topics' => $countTopic,
+            'Posts' => $countPosts,
+            'LastPost' => $lastPost,
+            'editLink' => $editLink,
+            'deleteLink' => $deleteLink,
+        );
+        $sections[] = $sectionRow;
     }
-//        echo "<td><a href=\"topic.php?sectionId=$sectionId\">" . $sectionValue["SectionName"] . "</a></td><td>" . $countTopic . "</td><td>";
-
-    $countPosts = 0;
-    $topicArray = array();
-    foreach ($resultTopic as $topicKey => $topicValue) {
-        $topicId = $topicValue["TopicId"];
-        $topicArray[] = $topicValue;
-        $countPosts = $comment->countCommentsByTopicId($topicValue["TopicId"]);
-//
-        $countPosts += $countPosts;
-
-
-//        echo $countPosts . "</td>";
-//        echo '<td>' . $topicArray[0]["UserName"] . '</td></tr>';
-    }
-    $lastPost = $topicArray[0]["UserName"];
-    $sectionRow = array(
-        'Section' => $sectionValue['SectionName'],
-        'Topics' => $countTopic,
-        'Posts' => $countPosts,
-        'LastPost' => $lastPost,
-        'editLink' => $editLink,
-        'deleteLink' => $deleteLink,
-    );
-    $sections[] = $sectionRow;
-
 }
