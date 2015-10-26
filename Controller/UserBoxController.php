7b2a266262b9dbcabee4bb2a18f77e25a3225930
Shaquille92 <slobodan.ned@gmail.com>
2015-10-26 16:13:28 +0200

    fixed userbox

--- Controller/UserBoxController.php
+++ Controller/UserBoxController.php
@@ -5,13 +5,10 @@
  * Date: 10/26/15
  * Time: 10:44 AM
  */
-//include ('../Utils/sessions.php');
-//include '../Model/AbstractModel.php';
-include ('../Model/User.php');
 
 $userData = new User();
 
-$userId = "1";
+$userId = $_SESSION['userId'];
 
 $getUserData = $userData->getUserDetail($userId);
 
