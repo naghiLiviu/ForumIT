4b06892ec802d027969b8630c71197ecba5f95c5
Isabela09 <branescu.isabela@gmail.com>
2015-10-27 11:23:03 +0200

    HeaderController cu logout

--- Controller/HeaderController.php
+++ Controller/HeaderController.php
@@ -6,5 +6,5 @@ if (!isset($_SESSION['userId']) && $_SESSION['userId'] == null) {
     $registerLink = '<a href="../View/register.php">Register</a> | ';
     $loginLink = '<a href="../View/login.php">LogIn</a>';
 }else{
-    $logoutLink = '<a href ="../logout.php">Log out</a>';
+    $logoutLink = '<a href ="../View/logout.php">Log out</a>';
 }
