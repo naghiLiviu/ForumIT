e6402f84f0a4b5cdc788b51864618954e0cf3fe8
naghiLiviu <liviu.naghi93@gmail.com>
2015-11-05 14:54:18 +0200

    forum object cleaned

--- Controller/deleteUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../Model/AbstractModel.php';
-include '../Model/User.php';
-
-$user = new User();
-
-$user->deleteUser($_GET["deleteUserId"]);
-
-header("Location: ../View/member.php");
--- Utils/sessions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: isabela
- * Date: 10/26/15
- * Time: 10:26 AM
- */
-
-session_start();
