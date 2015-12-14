daf00ba443c131bfa269a271d9b09c2eb0b7c28a
Isabela09 <branescu.isabela@gmail.com>
2015-12-14 11:15:30 +0200

    form with file browse added

--- module/Album/src/Album/Form/AlbumForm.php
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -9,6 +9,8 @@
 namespace Album\Form;
 
 use Zend\Form\Form;
+use Zend\Form\Element\Email;
+use Zend\InputFilter;
 
 class AlbumForm extends Form
 {
@@ -36,6 +38,49 @@ class AlbumForm extends Form
             ),
         ));
         $this->add(array(
+            'name' => 'username',
+            'type' => 'Text',
+            'attributes' => array('maxlength' => '12', 'size' => '32', 'type' => 'username'),
+            'options' => array(
+                'label' => 'Username',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'email',
+            'type' => 'Zend\Form\Element\Email',
+
+            'attributes' => array('maxlength' => '128', 'size' => '32', 'type' => 'email'),
+            'options' => array('label' => 'Email', 'appendText' => '@'),
+        ));
+        $this->add(array(
+            'name' => 'radio',
+            'type' => 'Zend\Form\Element\Radio',
+            'options' => array(
+                'label' => 'Do you like this ?',
+                'value_options' => array(
+                    'No' => 'No',
+                    'Yes' => 'Yes',
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'comment',
+            'type' => 'Zend\Form\Element\Textarea',
+            'attributes' => array('maxlength' => '250', 'type' => 'textarea'),
+            'options' => array(
+                'label' => 'Please insert your comment',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'fileUpload',
+            'type' => 'Zend\Form\Element\File',
+            'options' => array(
+                'label' => 'File Upload',
+            )
+        ));
+
+
+        $this->add(array(
             'name' => 'submit',
             'type' => 'Submit',
             'attributes' => array(
@@ -43,5 +88,37 @@ class AlbumForm extends Form
                 'id' => 'submitbutton',
             ),
         ));
+
+
+        //$this->addInputFilter();
+    }
+
+    public function addInputFilter()
+    {
+        $inputFilter = new InputFilter\InputFilter();
+
+        // File Input
+        $fileInput = new InputFilter\FileInput('fileUpload');
+        $fileInput->setRequired(true);
+
+        // You only need to define validators and filters
+        // as if only one file was being uploaded. All files
+        // will be run through the same validators and filters
+        // automatically.
+        $fileInput->getValidatorChain()
+            ->attachByName('filesize',      array('max' => 2048000))
+            //->attachByName('filemimetype',  array('mimeType' => 'image/png,image/x-png'))
+            ->attachByName('fileimagesize', array('maxWidth' => 1000, 'maxHeight' => 1000));
+
+        $fileInput->getFilterChain()->attachByName(
+            'filerenameupload',
+            array(
+                'target'    => './data/tmpuploads/avatar.png',
+                'randomize' => true,
+            )
+        );
+        $inputFilter->add($fileInput);
+
+        $this->setInputFilter($inputFilter);
     }
 }
